f784915369897857c13fbbc8b7ec55949551dd8d
Maegan Jong <maegan.jong@gmail.com>
2020-02-25 00:18:02 -0500

    edits

--- prog1.java
+++ prog1.java
@@ -41,7 +41,9 @@ class RandomWeight {
 	H.insert(0, dist[0]); // add	H to the heap
 
 	Random rand = new Random();
-
+	// generating a number [0,1]
+	rand.setSeed(long seed);
+	float value = rand.nextFloat()*(1+2.5e-16);
 
 	//Begin heap operations on remainder of Heap
 	while H.getSize() > 0	{
@@ -67,18 +69,25 @@ class RandomWeight {
 }
 
 public class MinHeap {
-  private int[] heap;
+  private float[][] heap;
 	private int size;
 	private int max;
+	private int[] search;
 
 	public MinHeap (int max) {
 		this.max = max;
 		this.size = 0;
-		heap = new int[this.max + 1][2];
-		heap[0] = 0;
+		//do we have to initialize a 2d array first... or do we just add it as we go... so that it becomes an array of arrys
+		this.heap = new float[this.max + 1][2];
+		this.heap[0] = {0, Integer.MIN_VALUE};
+		this.search = new int[this.max+1];
+		for(int i=0; i<this.search.length; i++)
+		{
+    	this.search[i] = null;
+		}
 	}
   public static float peek() {
-		return heap[1];
+		return this.heap[0][1];
   }
 
 	public int getSize(){
@@ -90,7 +99,7 @@ public class MinHeap {
 		int l = 2*N;
 		int r = 2*N+1;
 		int smallest;
-		if (this.heap[r]) && (this.heap[r][1] < this.heap[N][1]) {
+		if (this.heap[r]) && (this.heap[r][1] < this.heap[N][1]) { //** how to check if it exists?? **//
 			smallest = r;
 		}
 		else {
@@ -119,20 +128,27 @@ public class MinHeap {
   }
 
 	public insert(int v, float d) {
-		this.size += 1;
-		this.heap[this.size] = v;
-		int N = this.size;
+		// if the item is already in the search array (this means that it's in the heap) then we only update the new position in the heap after we
+		// heapify with the changed value
+		// now looking through the code to figure out where we actually compare the weights of the edges...
+		int N = 0;
+		if (!this.search[v]){ //if the node is not yet in the heap - and the v index of the search is NULL
+			this.size += 1; //we increase the size of the heap to add in another
+			this.heap[this.size] = new int[2];
+			this.heap[this.size] = {v, d};
+			N = this.size;
+		}
+		else { //it is in the heap
+			N = this.search[v];
+			this.heap[N][1] = d; //adjusted the value
+		}
 		while (N > 0) && (this.heap[N/2] < this.heap[N]) {
 			int a = this.heap[N/2];
 			this.heap[N/2] = this.heap[N];
 			this.heap[N] = a;
 			N /= 2;
 		}
-
-		// if already in, then you would just change the value
-
-
-		// SEPARATE ARRAY - index of a value is a vertex, value os the location of that vertex within the heap ---- SEARCH FOR IT
+		this.search[v] = N;
 	}
 }
 // Class for RandomSquare
