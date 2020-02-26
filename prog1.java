d09259872312ad58481292e98248edb601792ba4
Maegan Jong <maegan.jong@gmail.com>
2020-02-25 22:09:26 -0500

    edit

--- prog1.java
+++ prog1.java
@@ -1,64 +1,116 @@
 import java.io.*;
 import java.util.Random;
+import java.lang.Math;
 
 class RandomWeight {
 	public static void main (String[] args) {
-		System.out.println("Hello Java");
 
-    // args[1] - 0, args[2] - numpoints, args[3] - numtrials, args[4] - dimension
+    // args[0] - 0, args[1] - numpoints, args[2] - numtrials, args[3] - dimension
+
+		private static float weight (int v, int w) {
+			if (dim == 0){
+				Random rand = new Random();
+				// generating a number [0,1]
+				long seed = System.currentTimeMillis() % 1000; //define to something random
+				rand.setSeed(seed);
+				float value = rand.nextFloat()*(1+2.5e-16);
+				return value;
+			}
+			// if dim is higher, calculate distance between them
+			else {
+				float d = 0; // running count for distance
+				for (int i = 0; i < dim; i++ ){
+					dist += (V[v][i] - V[v][j]) * (V[v][i] - V[v][j]);
+				}
+				return Math.sqrt(d);
+
+			}
 
 
-	int n = 5;
-	// initializing graph, with vertices stored in array V, consisting of integers that index each vertex
+		}
 
-	int V[] = new int[n];
-	for (int i = 0; i < V.length; i++)	{
-		V[i] = i;
-	}
+		int n = Integer.parseInt(args[1]);
+		int trials = 1; //Integer.parseInt(args[2]);
+		int dim = 0; // Integer.parseInt(args[3]);
+		// initializing graph, with vertices stored in array V, consisting of integers that index each vertex
 
-	// array of (conservative) minimum distance to vertex
-	int dist[] = new float[n];
-	for (int i = 0; i < V.length; i++)	{
-		V[i] = n; // n-1 edges in MST, max weight of an edge is 1
-	}
+		// IMPLEMENT TRILAS
 
-	// array of pointers (indices) to previous vertex in MST, initialized to a null value
-	int prev[] = new int[n];
-	for (int i = 0; i < V.length; i++)	{
-		V[i] = -1; // an "null" pointer
-	}
+		if (dim == 0) {
+			int V[] = new int[n];
+			for (int i = 0; i < V.length; i++)	{
+				V[i] = i;
+			}
 
-	// set of vertices S for cut property, 1 if in S and 0 otherwise
-	int S[] = new int[n]; // initialized to 0
-
-	// start with 0-indexed vertex in S, so its distance to itself is 0
-	S[0] = 1;
-  dist[0] = 0;
-
-	MinHeap H = new MinHeap(n); // heap of lightest n edges at any given time
-	MinHeap tempHeap = new MinHeap(2*n); // temporary heap to contain added edges
-
-	H.insert(0, dist[0]); // add	H to the heap
-
-	Random rand = new Random();
-	// generating a number [0,1]
-	long seed = 1232323232; //define to something random
-	rand.setSeed(seed);
-	float value = rand.nextFloat()*(1+2.5e-16);
-
-	//Begin heap operations on remainder of Heap
-	while (H.getSize() > 0)	{
-		int v = extractMin(H);
-		S[v] = 1; // add v to set S
-		for (int w = 0; w < V.length && S[i] == 0; i++) { // checking for vertices w not in S
-			float weight(v,w) = rand.nextDouble(); // REMOVE!!!!!!!!!!!!!!!!!!!
-			if (dist[w] > weight(v,w)){
-				dist[w] = weight(v,w);
-				prev[w] = v;
-				H.insert(w, dist[w]);
+			// array of (conservative) minimum distance to vertex
+			int dist[] = new float[n];
+			for (int i = 0; i < V.length; i++)	{
+				V[i] = n; // n-1 edges in MST, max weight of an edge is 1
 			}
 
 		}
+		// for higher dimensions
+		else {
+			float V[][] = new float[n][dim];
+			for (int i = 0; i < V.length; i++)	{
+				for (int j = 0; j < dim; j++) {
+					Random rand = new Random();
+					// generating a number [0,1]
+					long seed = System.currentTimeMillis() % 1000; //define to something random
+					rand.setSeed(seed);
+					float value = rand.nextFloat()*(1+2.5e-16);
+					V[i][j] = value;
+				}
+			}
+
+			int dist[] = new float[n];
+			for (int i = 0; i < V.length; i++)	{
+				V[i] = n * n; // n-1 edges in MST, max weight of an edge is sqrt n < n
+
+		}
+
+
+		// array of pointers (indices) to previous vertex in MST, initialized to a null value
+		int prev[] = new int[n];
+		for (int i = 0; i < V.length; i++)	{
+			V[i] = -1; // an "null" pointer
+		}
+
+		// set of vertices S for cut property, 1 if in S and 0 otherwise
+		int S[] = new int[n]; // initialized to 0
+
+		// start with 0-indexed vertex in S, so its distance to itself is 0
+		S[0] = 1;
+	  dist[0] = 0;
+
+		MinHeap H = new MinHeap(n); // heap of lightest n edges at any given time
+		MinHeap tempHeap = new MinHeap(2*n); // temporary heap to contain added edges
+
+		H.insert(0, dist[0]); // add	H to the heap
+
+		//Begin heap operations on remainder of Heap
+		while (H.getSize() > 0)	{
+			int v = extractMin(H);
+			S[v] = 1; // add v to set S
+			for (int w = 0; w < V.length && S[i] == 0; i++) { // checking for vertices w not in S
+				// float weight(v,w) = rand.nextDouble(); // REMOVE!!!!!!!!!!!!!!!!!!!
+				if (dist[w] > weight(v,w)){
+					dist[w] = weight(v,w);
+					prev[w] = v;
+					H.insert(w, dist[w]);
+				}
+
+			}
+
+			// Calculate average weight
+			float total = 0.0;
+
+			for (int i = 0; i < V. length; i++) {
+				total += dist[i];
+
+			}
+
+			System.out.println(total / n);
 
 
 	}
@@ -68,6 +120,8 @@ class RandomWeight {
 	// if smallest edge already in MST, go for second smallest? - Mike
 	}
 }
+
+
 
 public class MinHeap {
   private float[][] heap;
